abff4d02c43e708777ff33ca4e580ccde470817d
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-04 02:52:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+/act/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+/undoing/
 end
 
 def words_five_letters_long(text)
-
+/trick/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+/Billy!/
 end
 
 def valid_phone_number?(phone)
-
+/979-587-8056/
 end
